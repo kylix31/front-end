1885a9f1f920fed49020f4d374f05a03141fce2f
Raul Luzetti <raul.chiodi@gmail.com>
2021-02-27 22:50:21 -0300

    ENDED

--- src/pages/currentOrder/currentOrder.tsx
+++ src/pages/currentOrder/currentOrder.tsx
@@ -1,19 +1,37 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from 'react-query';
-import { CircularProgress, CardContent, Grid } from '@material-ui/core';
-import { makeStyles, createMuiTheme } from '@material-ui/core/styles';
+import {
+  CircularProgress,
+  CardContent,
+  Grid,
+  Divider,
+} from '@material-ui/core';
+import {
+  makeStyles,
+  ThemeProvider,
+  createMuiTheme,
+} from '@material-ui/core/styles';
 
 import { Card } from '../../components/Card/Card';
 import { Container } from '../../components/Container/Container';
+import { toCurrency } from '../../core/number';
 
-import { DesktopSteps } from './steps';
+import { DesktopSteps, MobileSteps } from './steps';
 
 import { API_URL } from '../../env';
 
 interface ParamsType {
   id: string;
 }
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#000000',
+    },
+  },
+});
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -28,8 +46,52 @@ const useStyles = makeStyles(theme => ({
   cardTitle: {
     textAlign: 'center',
     textTransform: 'uppercase',
-    [theme.breakpoints.down('xs')]: {
+    [theme.breakpoints.down('sm')]: {
       textAlign: 'start',
+    },
+  },
+  desktopSteps: {
+    display: 'block',
+    [theme.breakpoints.down('sm')]: {
+      display: 'none',
+    },
+  },
+  mobileSteps: {
+    display: 'none',
+    [theme.breakpoints.down('sm')]: {
+      display: 'block',
+    },
+  },
+  productList: {
+    '& h3, & h5': {
+      fontWeight: 200,
+    },
+  },
+  summaryContainer: {
+    padding: theme.spacing(2),
+    '& h3': {
+      marginBlock: 0,
+      fontWeight: 100,
+      textTransform: 'uppercase',
+      [theme.breakpoints.down('sm')]: {
+        fontSize: '1rem',
+      },
+    },
+    '& h2': {
+      textTransform: 'uppercase',
+    },
+  },
+  lastContainerGrid: {
+    padding: theme.spacing(2),
+    justifyContent: 'space-evenly',
+    '& h5': {
+      marginBlock: '1em',
+    },
+    '& h6': {
+      marginBlock: '0',
+    },
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: 'column',
     },
   },
 }));
@@ -51,27 +113,123 @@ export const CurrentOrder = () => {
       </div>
     );
 
-  console.log(data);
+  if (error) return <div>Alguma coisa bugou...</div>; //But will not 😁
 
   return (
     <div className={classes.root}>
-      <Container>
-        <Card>
-          <CardContent>
-            <Grid container direction="column">
-              <Grid className={classes.cardTitle} item>
-                <h1>AGORA VAI, {data.name}!</h1>
+      <ThemeProvider theme={theme}>
+        <Container>
+          <Card>
+            <CardContent>
+              <Grid container spacing={4} direction="column">
+                <Grid className={classes.cardTitle} item>
+                  <h1>AGORA VAI, {data.name}!</h1>
+                </Grid>
+                <Grid className={classes.cardTitle} item>
+                  <h2>NÚMERO DO PEDIDO: {data.id}</h2>
+                </Grid>
+                <Grid item>
+                  <Grid container justify="center">
+                    <Grid item>
+                      <DesktopSteps
+                        className={classes.desktopSteps}
+                        step={data.status}
+                      />
+                      <MobileSteps
+                        step={data.status}
+                        className={classes.mobileSteps}
+                      />
+                    </Grid>
+                  </Grid>
+                </Grid>
+                <Grid item>
+                  <h3 style={{ textAlign: 'center' }}>RESUMO DA COMPRA</h3>
+                </Grid>
+                <Grid item>
+                  {data.items.map((item: any, index: number) => (
+                    <div key={index}>
+                      <Divider />
+                      <Grid
+                        className={classes.productList}
+                        container
+                        justify="space-around"
+                      >
+                        <Grid item>
+                          <h3>{`${item.qty}x  ${item.name}`}</h3>
+                        </Grid>
+                        <Grid item>
+                          <h5>{toCurrency(item.price * item.qty)}</h5>
+                        </Grid>
+                      </Grid>
+                    </div>
+                  ))}
+                  <Divider />
+                </Grid>
+                <Grid
+                  className={classes.summaryContainer}
+                  container
+                  direction="column"
+                >
+                  <Grid item container justify="space-between">
+                    <Grid item>
+                      <h3>Prazo de entrega:</h3>
+                    </Grid>
+                    <Grid item>
+                      <h3>{`de ${data.freight.from} a ${data.freight.to} dias`}</h3>
+                    </Grid>
+                  </Grid>
+                  <Grid item container justify="space-between">
+                    <Grid item>
+                      <h3>Frete:</h3>
+                    </Grid>
+                    <Grid item>
+                      <h3>{toCurrency(data.freight.price)}</h3>
+                    </Grid>
+                  </Grid>
+                  <Grid item container justify="space-between">
+                    <Grid item>
+                      <h2>Total:</h2>
+                    </Grid>
+                    <Grid item>
+                      <h2>{toCurrency(data.total)}</h2>
+                    </Grid>
+                  </Grid>
+                </Grid>
+                <Divider />
+                <Grid
+                  className={classes.lastContainerGrid}
+                  container
+                  spacing={2}
+                >
+                  <Grid item>
+                    <Grid container direction="column">
+                      <Grid item>
+                        <h5>ENTREGAR EM:</h5>
+                      </Grid>
+                      <Grid item>
+                        <h6>{`${data.address.street}, ${data.address.number}`}</h6>
+                      </Grid>
+                    </Grid>
+                    <Grid item>
+                      <h6>{`${data.address.city} - ${data.address.state} - ${data.address.postcode}`}</h6>
+                    </Grid>
+                  </Grid>
+                  <Grid item>
+                    <Grid container>
+                      <Grid item>
+                        <h5>FORMA DE PAGAMENTO:</h5>
+                        <Grid item>
+                          <h6>{data.payment_method}</h6>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                </Grid>
               </Grid>
-              <Grid className={classes.cardTitle} item>
-                <h2>NÚMERO DO PEDIDO: {data.id}</h2>
-              </Grid>
-              <Grid item>
-                <DesktopSteps step={data.status} />
-              </Grid>
-            </Grid>
-          </CardContent>
-        </Card>
-      </Container>
+            </CardContent>
+          </Card>
+        </Container>
+      </ThemeProvider>
     </div>
   );
 };
--- src/pages/currentOrder/steps.tsx
+++ src/pages/currentOrder/steps.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Step, Stepper, StepLabel } from '@material-ui/core';
+import { Step, Stepper, StepLabel, StepConnector } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 
 const STEPS = getSteps();
 
 type AppProps = {
   step: string;
+  className?: string;
 };
 
 enum Steps {
@@ -25,12 +27,71 @@ function getSteps() {
   ];
 }
 
-export const DesktopSteps = ({ step }: AppProps) => {
-  console.log(Steps[step]);
+const Connector = withStyles({
+  alternativeLabel: {
+    top: 10,
+    left: 'calc(-50% + 16px)',
+    right: 'calc(50% + 16px)',
+  },
+  active: {
+    '& $line': {
+      borderColor: 'black',
+    },
+  },
+  completed: {
+    '& $line': {
+      borderColor: 'black',
+    },
+  },
+  line: {
+    borderColor: '#eaeaf0',
+    borderTopWidth: '4px',
+  },
+})(StepConnector);
 
+// function MyCustomStepLabel(props: StepIconProps) {
+//   const classes = useStyles();
+//   const { active, completed } = props;
+
+//   return (
+//     <div
+//       className={clsx(classes.root, {
+//         [classes.active]: active,
+//       })}
+//     >
+//       {completed ? (
+//         <div className={classes.completed} />
+//       ) : (
+//         <div className={classes.circle} />
+//       )}
+//     </div>
+//   );
+// }
+
+export const DesktopSteps = ({ step, className }: AppProps) => {
   return (
-    <div>
-      <Stepper activeStep={Steps[step]}>
+    <div className={className}>
+      <Stepper
+        activeStep={Steps[step]}
+        connector={<Connector />}
+        alternativeLabel
+      >
+        {STEPS.map((label, index) => {
+          return (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          );
+        })}
+      </Stepper>
+    </div>
+  );
+};
+
+export const MobileSteps = ({ step, className }: AppProps) => {
+  return (
+    <div className={className}>
+      <Stepper activeStep={Steps[step]} orientation="vertical">
         {STEPS.map((label, index) => {
           return (
             <Step key={label}>
